85bc1f47cba3d9dbf62313f0ec3a0a9a910722e2
EduhG <edwinnyangena@gmail.com>
2016-10-09 10:45:43 +0300

    created sold_item layout to add to recycler view

--- app/src/main/java/com/eduhg/myshop/fragments/DashboardFragment.java
+++ app/src/main/java/com/eduhg/myshop/fragments/DashboardFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 import com.eduhg.myshop.R;
 
 public class DashboardFragment extends Fragment {
+    RecyclerView mRecyclerView;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -28,6 +30,10 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+
+        return rootView;
     }
 }
