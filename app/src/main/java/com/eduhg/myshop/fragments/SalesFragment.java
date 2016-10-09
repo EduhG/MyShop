ef0d25e1f55728104edfb744dc1e9f815edd6b12
EduhG <edwinnyangena@gmail.com>
2016-10-09 10:23:36 +0300

    added popup menu to saales fragment

--- app/src/main/java/com/eduhg/myshop/fragments/SalesFragment.java
+++ app/src/main/java/com/eduhg/myshop/fragments/SalesFragment.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.eduhg.myshop.R;
 
@@ -18,6 +23,7 @@ public class SalesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
 
     }
 
@@ -25,6 +31,51 @@ public class SalesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sales, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sales, container, false);
+
+        final ImageButton imageButton = (ImageButton) rootView.findViewById(R.id.soldItems);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_LONG).show();
+                showPopupMenu(view);
+            }
+        });
+
+        return rootView;
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(getActivity(), v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.sales_actions, popup.getMenu());
+        popup.show();
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupmenu = new PopupMenu(getActivity(), view);
+        popupmenu.getMenuInflater().inflate(R.menu.sales_actions, popupmenu.getMenu());
+        popupmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuitem) {
+                switch (menuitem.getItemId()) {
+                    case R.id.action_search:
+                        Toast.makeText(getActivity(), "Search Clicked", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.action_settings:
+                        Toast.makeText(getActivity(), "Settings Clicked", Toast.LENGTH_LONG).show();
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+                return true;
+            }
+        });
+
+        popupmenu.show();
     }
 }
